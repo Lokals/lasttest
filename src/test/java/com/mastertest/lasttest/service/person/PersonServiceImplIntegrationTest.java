67a181ea72b1d40bf01b885850c5fe3bdccebb0e
Lokals <kowalewskyop@gmail.com>
2024-01-09 20:11:59 +0100

    by accident deleted

---
+++ src/test/java/com/mastertest/lasttest/service/person/PersonServiceImplIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.mastertest.lasttest.service.person;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+class PersonServiceImplIntegrationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void update() {
+    }
+}
